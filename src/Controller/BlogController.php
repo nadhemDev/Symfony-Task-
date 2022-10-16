414e820166e92293664efe187eef1ab05df8eeda
NadhemDev <blendplay2@gmail.com>
2022-10-16 13:10:49 +0100

    twig fixed

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BlogController extends AbstractController
+{
+    /**
+     * @Route("/blog", name="app_blog")
+     */
+    public function index(): Response
+    {
+        return $this->render('blog/index.html.twig', [
+            'controller_name' => 'BlogController',
+        ]);
+    }
+    /**
+     * @Route ("/", name="home")
+     */
+    public function home()
+    {
+        return $this->render('blog/home.html.twig',[
+            'title' => "Bienvenue ici les amis !",
+            'age' => 31
+        ]);
+        
+    }
+
+ 
+}
